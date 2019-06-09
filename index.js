6c7aa9e5887c7f4266f8a2468d60741b0e0d51a4
Arif BAĞLAR <arifbaglar@hotmail.com>
2019-06-09 13:40:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file
-function findMatching(drivers,name){
-  drivers.filter(function(a){return a === name;});
+function findMatching(array,name){
+  array.filter(function(a){return a === name;});
 }
