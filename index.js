3d90ec9b2255cdb7029c1b6d0cbe895a972a7916
Arif BAĞLAR <arifbaglar@hotmail.com>
2019-06-09 13:43:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,13 @@
 function findMatching(array,name){
   array.filter(function(a){return a === name;});
 }
+
+function colorFilter (collection, name) {
+  for (const user in array) {
+    if (user === name) {
+      console.log(user.firstName);
+    }
+  }
+}
+ 
+colorFilter(users, 'Red');
