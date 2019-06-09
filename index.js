53f25049a6c381456b933f8243f485150241580f
Arif BAĞLAR <arifbaglar@hotmail.com>
2019-06-09 13:49:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 // Code your solution in this file
-function findMatching(array,name){
-  array.filter(function(a){return a === name;});
-}
-
 function colorFilter (collection, name) {
-  for (const user in array) {
+  for (const user of array) {
     if (user === name) {
       console.log(user.name);
     }
