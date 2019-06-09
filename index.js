37192639428d2a589a822326bf5f4895d98b624e
Arif BAĞLAR <arifbaglar@hotmail.com>
2019-06-09 14:05:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 // Code your solution in this file
-function findMatching(array, name) {
-  for (const user of array) {
-    if (user === name) {
-       user;
-    }
-  }
-}
  function findMatching (array, name) {
   const newCollection = [];
- 
   for (const user of array) {
-    if (cb(user)) {
+    if (user === name) {
       newCollection.push(user);
     }
   }
